6ef25becc60d7f0b1d12f0541c063916ed20307f
pl.rachuna-net-dragoslaw <service_account_group_105046057_f2437bed1abe0c1dba11c0311ed31b2d@noreply.gitlab.com>
2026-04-02 20:12:01 +0000

    feat: inventory 26 vault policies as .tf.json

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "https://vault-1022.rachuna-net.pl:8200"
+}
