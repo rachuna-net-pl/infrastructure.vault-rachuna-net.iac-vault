ea4c9419d40abb0c6d18cc1bffa6a05e481cf309
Andrzej <tech-mrachuna-orchiestrator@rachuna-net.pl>
2026-04-03 12:40:02 +0000

    feat: add dragoslawa userpass with random password in users/ KV

--- providers.tf
+++ providers.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/vault"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
   }
   backend "http" {}
 }
